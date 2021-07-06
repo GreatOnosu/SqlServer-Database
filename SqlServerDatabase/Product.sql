8f92e43c8292b0ad6cfc287425ea446801759ecc
Onosu Great <onosugreat@gmail.com>
2021-07-06 13:19:29 +0100

    master

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,6 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL,
-    [animal] NVARCHAR(150) NULL
+    [Color] NVARCHAR(50) NULL
 )
