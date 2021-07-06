8a721c32074bd0dc829bd723fa8ae842eb3f01ce
Onosu Great <onosugreat@gmail.com>
2021-07-06 13:22:12 +0100

    dev

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -4,5 +4,6 @@
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
     [Color] NVARCHAR(50) NULL,
-    [animal] NVARCHAR(150) NULL
+    [animal] NVARCHAR(150) NULL,
+    [food] NVARCHAR(100) NULL
 )
